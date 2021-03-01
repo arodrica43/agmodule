c828119cbe7e35a3100c63eeb3ed1ad6635eb685
Alexº <lexws33@gmail.com>
2021-03-01 16:44:57 +0100

    Deploy

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,3 @@
-from django.db import models
-
-# Create your models here.
+from apps.core.mechanics import *
+from apps.core.profiles import *
+from apps.core.utils import *
---
+++ apps/core/profiles.py
@@ -0,0 +1,101 @@
+from django.db import models
+from django.contrib.auth.models import User
+from enum import Enum
+from enumfields import EnumField
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MaxValueValidator, MinValueValidator
+#from django.db.models.manager import EmptyManager
+from jsonfield import JSONField
+
+# User-related models
+
+class EmotionProfile(models.Model):
+    valence =  models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=0)
+    arousal =  models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=0)
+
+class GamerProfile(models.Model):
+    #user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    disruptor = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=1)
+    free_spirit = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=1)
+    achiever = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=1)
+    player = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=1)
+    socializer = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=1)
+    philantropist = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=1)
+    no_player = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)],default=0)
+    data = JSONField(default = dict)
+ 
+    associated_mechanic = {
+        'disruptor':'Change',
+        'free_spirit':'Autonomy',
+        'achiever':'Mastery',
+        'player':'Reward',
+        'socializer':'Relatedness',
+        'philantropist':'Purpose',
+        'no_player':'',
+        -1: 'Unknown',
+        0:'Change',
+        1:'Autonomy',
+        2:'Mastery',
+        3:'Reward',
+        4:'Relatedness',
+        5:'Purpose',
+        6:''
+    }
+
+    def vectorize(self):
+        return (self.disruptor,self.free_spirit,self.achiever,self.player,self.socializer,self.philantropist,self.no_player)
+
+class SocialProfile(models.Model):
+
+    class AvatarType(Enum):   # A subclass of Enum
+        art = "art"
+        diamond = "diamond"
+        games = "games"
+        money = "money"
+        music = "music"
+        photo = "photo"
+        science = "science"
+        tech = "tech" 
+        user = "user"
+        Unknown = "Unknown"
+    
+    image = EnumField(AvatarType,max_length=11,default = AvatarType.diamond)
+    description = models.TextField(default="")
+    data = JSONField(default = list)       
+
+
+def username_exists(value):
+    qs = Gamer.objects.filter(user__username = value)
+    if len(qs) == 0:
+        raise ValidationError(
+            _('%(value)s is not an existing username'),
+            params={'value': value},
+        )
+
+def unique_individual_group(value):
+    #print("Hey Hey")
+    #print(value)
+    #print(value['groups'])
+    if 'groups' in value.keys():
+        groups =  value['groups']
+        #print("groups: ",groups)
+        for g in groups:
+            if len(g.name) >= 10:
+                if g.name[:10] == 'individual' and g.name[10:] != '_' + value['username']:
+                    raise ValidationError(
+                        _('An user cannot be in an individual group other than its default'),
+                        params={'value': value},
+                    )
+    else:
+        raise ValidationError(
+                        _('Every user should be in its own individual group. Automathically added.'),
+                        params={'value': value},
+                    )
+
+class Gamer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    emotion_profile = models.OneToOneField(EmotionProfile, on_delete=models.CASCADE)
+    gamer_profile =  models.OneToOneField(GamerProfile, on_delete=models.CASCADE)
+    social_profile =  models.OneToOneField(SocialProfile, on_delete=models.CASCADE)
+
