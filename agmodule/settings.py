3f7edf7c938a82f1b2d66e35a90b1ffd280a4a02
Alexº <lexws33@gmail.com>
2021-03-01 20:08:43 +0100

    Deploy

--- agmodule/settings.py
+++ agmodule/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='!c19@b=qkey^k*m2&y@b$mk*8+%h&-#h=3!a30dqbfqq=0_d#z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
