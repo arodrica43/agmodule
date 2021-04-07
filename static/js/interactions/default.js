bf92d870f4d2a5a3ef0873b5bfca61a5081c25d9
Alexº <lexws33@gmail.com>
2021-04-07 16:58:49 +0200

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -214,7 +214,7 @@ try {
                                                     '<span id="star-4-dynamic_index" onclick="valorate(4);" class="fa fa-star" style="cursor:pointer; font-size: calc(0.8em + 0.8vw);"></span>'+
                                                     '<span id="star-5-dynamic_index" onclick="valorate(5);" class="fa fa-star" style="cursor:pointer; font-size: calc(0.8em + 0.8vw);"></span><br>'+
                                                 '</p>' + 
-                                                 '<br><a href="' + link_url + '" style="position: absolute;bottom: -20px;right: 0;cursor:pointer; font-size: calc(0.7em + 0.8vw);">Discover more \>  <br> </a>',
+                                                 '<br><a href="' + link_url.replace(/\s/g, "+"); + '" style="position: absolute;bottom: -20px;right: 0;cursor:pointer; font-size: calc(0.7em + 0.8vw);">Discover more \>  <br> </a>',
                                                   valorate(res_json['results'])))
         .catch(error => (console.log("Error: " + error)))
                 
