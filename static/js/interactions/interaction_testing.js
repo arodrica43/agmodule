0f1751f3d705e239c93102525991fe70ad180542
Alexº <lexws33@gmail.com>
2021-04-30 16:20:39 +0200

    Deploy

--- static/js/interactions/interaction_testing.js
+++ static/js/interactions/interaction_testing.js
@@ -89,7 +89,7 @@ function startBot(main_content_name, interaction_speed, expected_valoration) {
         var type_idx = Math.floor(Math.random() * all_interactables.length);
         var elem_idx = Math.floor(Math.random() * all_interactables[type_idx].length);
         
-        interact(type_idx, elem_idx, expected_valoration);
+        //interact(type_idx, elem_idx, expected_valoration);
         
 
     }, interaction_speed*100, "JavaScript"); // EXTERNAL VAR :: interaction speed
