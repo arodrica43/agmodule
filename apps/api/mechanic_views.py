e311ce6804b13e5c65fb9735356b2872e9e42486
Alexº <lexws33@gmail.com>
2021-05-07 16:22:41 +0200

    Deploy

--- apps/api/mechanic_views.py
+++ apps/api/mechanic_views.py
@@ -107,7 +107,7 @@ class GMechanicViewSet(viewsets.ModelViewSet):
                             lock.release()
                             raise Http404
                     #Pre variables :: Before update --------------------------------------------
-                    pre_log = statistic[0].log
+                    pre_log = statistic[0].log.copy()
                     # --------------------------------------------------------------------------
                     for arg in ['history', 'main_time', 'focus_time', 'interaction_time','hidden_content_time', 'shown_content_time', 'valoration']:
                         uplog = statistic[0].log
