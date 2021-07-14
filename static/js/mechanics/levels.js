ec3a6a883001aa37c2c1e8d0609d5df71285ea42
Alexº <lexws33@gmail.com>
2021-07-14 18:46:12 +0200

    Deploy

--- static/js/mechanics/levels.js
+++ static/js/mechanics/levels.js
@@ -40,5 +40,16 @@
         });
 
 
+    $(".meter > span").each(function () {
+      $(this)
+        .data("origWidth", $(this).width())
+        .width(0)
+        .animate(
+          {
+            width: $(this).data("origWidth")
+          },
+          1200
+        );
+    });
 
       
