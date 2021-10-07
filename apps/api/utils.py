ad9984de71c6f312c1a4c9b9a9f93663c087ea28
Alexº <lexws33@gmail.com>
2021-10-07 10:28:42 +0200

    Deploy

--- apps/api/utils.py
+++ apps/api/utils.py
@@ -487,11 +487,7 @@ def get_interaction_index(request, username, mechanic_id):
     lock.acquire()
     try:
         gm =  GMechanic.objects.filter(id = mechanic_id)
-        print(gm)
-        if len(gm) > 0:
-            gm = gm[0]
-        user_stats = InteractionStatistic.objects.filter(user = username, mechanic = gm)
-        print("User Stats", user_stats)
+        user_stats = InteractionStatistic.objects.filter(user = username, mechanic = gm[0])
         try:
             demand = user_stats[0]
         except:
