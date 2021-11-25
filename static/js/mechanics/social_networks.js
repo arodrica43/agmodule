8e73bdb5e764561b8416336074580ac0155600e0
Alexº <lexws33@gmail.com>
2021-11-25 17:56:36 +0100

    Deploy

--- static/js/mechanics/social_networks.js
+++ static/js/mechanics/social_networks.js
@@ -74,9 +74,11 @@ function selectAvatar(name){
   log_click({itime: 1, message:"Select avatar " + name + " button (settings modal) clicked", register : log, level:2,type:"ButtonClick"});
   // --------------------------------------------------------------------------
   avatars = document.querySelectorAll("img[id*='avatar']")
+  console.log(avatars);
   for(var i = 0; i < avatars.length; i++){
     document.getElementById("avatar-" + avatars[i].name).style = "width:80%;";
   }
+  console.log(name);
   document.getElementById("avatar-" + name).style = "width:100%; opacity:1;";
   document.querySelector("#profile-img").src = "https://agmodule.herokuapp.com/media/avatars/" + name + ".png";
   document.querySelector("#profile-img").name = name;
