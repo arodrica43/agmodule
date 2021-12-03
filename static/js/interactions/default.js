028aa20fc727644b4d488d600da6bf639f37e8ed
Alexº <lexws33@gmail.com>
2021-12-03 19:05:43 +0100

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -232,7 +232,7 @@ try {
 																								 '.grow:hover { transform: scale(1.3); }</style>' + 
         										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;"> ' + 
         											'<img style="width:10%;float:right; margin-right:calc(50px + 6vw);" src="https://agmodule.herokuapp.com/media/avatars/' + res_json.avatar.slice(res_json.avatar.indexOf(".") + 1, res_json.avatar.length) + '.png">' +
-        											//'<p style="font-size:calc(10px + 1vw);padding:10px;">Has obtingut un premi: ' + res_json.gmtype + '</p>' +
+        											'Has obtingut un premi: ' + res_json.gmtype +
         											'<img style="width:7%;" src="https://agmodule.herokuapp.com/media/dashboard_icons/badges.png">  </p>' +
         										'<p style="position: absolute; bottom: calc(-60px + 0vw); left: 0;font-size:calc(10px + 1vw);">T\'ha agradat el premi?</p>' +	
         										'<p style="position: absolute; bottom: calc(-90px + 0vw); left: 0;">'+
