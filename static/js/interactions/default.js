0fa74545500ece93c12bfc0b746ba91e2489709b
Alexº <lexws33@gmail.com>
2021-12-03 19:37:12 +0100

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -248,7 +248,11 @@ try {
         .then(res_json => (console.log(res_json), document.querySelector(id).innerHTML += '<style>.grow { transition: all .2s ease-in-out; }' +
 																								 '.grow:hover { transform: scale(1.3); }</style>' + 
         										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;"> ' + 
-        											'<img style="width:10%;float:right; margin-right:calc(50px + 6vw);" src="https://agmodule.herokuapp.com/media/avatars/' + res_json.avatar.slice(res_json.avatar.indexOf(".") + 1, res_json.avatar.length) + '.png">' +
+        											'<img style="width:10%;float:left; margin-right:calc(50px + 6vw);" src="https://agmodule.herokuapp.com/media/avatars/' + res_json.avatar.slice(res_json.avatar.indexOf(".") + 1, res_json.avatar.length) + '.png">' +
+        											'<img style="width:7%;" src="https://agmodule.herokuapp.com/media/dashboard_icons/badges.png">' +
+        											'<div>Has obtingut un premi: ' + GMLabels[res_json.gmtype] + '</div></p>' +
+        										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;"> ' + 
+        											'<img style="width:10%;float:left; margin-right:calc(50px + 6vw);" src="https://agmodule.herokuapp.com/media/avatars/' + res_json.avatar.slice(res_json.avatar.indexOf(".") + 1, res_json.avatar.length) + '.png">' +
         											'<img style="width:7%;" src="https://agmodule.herokuapp.com/media/dashboard_icons/badges.png">' +
         											'Has obtingut un premi: ' + GMLabels[res_json.gmtype] + '</p>' +
         										'<p style="position: absolute; bottom: calc(-60px + 0vw); left: 0;font-size:calc(10px + 1vw);">T\'ha agradat el premi?</p>' +	
