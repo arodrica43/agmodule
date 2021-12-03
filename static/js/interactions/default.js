7648b675725093f8ed995b6cd05f242351227bf1
Alexº <lexws33@gmail.com>
2021-12-03 20:09:05 +0100

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -251,10 +251,10 @@ try {
         											'<img style="width:10%;float:left; margin-right:calc(50px + 6vw);" src="https://agmodule.herokuapp.com/media/avatars/' + res_json.avatar.slice(res_json.avatar.indexOf(".") + 1, res_json.avatar.length) + '.png"></p>' +
         											//'<img style="width:7%;" src="https://agmodule.herokuapp.com/media/dashboard_icons/badges.png">' +
         											//'Has obtingut un premi: ' + GMLabels[res_json.gmtype] + '</p>' +
-        										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;text-align:center;"> ' + 
+        										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;left:calc(5% + 1vw);top:calc(2.5vw);"> ' + 
         											'<img style="width:7%;" src="https://agmodule.herokuapp.com/media/dashboard_icons/badges.png"></p>' +
-        										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;text-align:center;"> ' + 
-        											'Has obtingut un premi: ' + GMLabels[res_json.gmtype] + '</p>' +
+        										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;left:calc(10% + 1vw);"> ' + 
+        											'Has obtingut un premi del tipus \"' + GMLabels[res_json.gmtype] + '\"</p>' +
         										'<p style="position: absolute; bottom: calc(-60px + 0vw); left: 0;font-size:calc(10px + 1vw);">T\'ha agradat el premi?</p>' +	
         										'<p style="position: absolute; bottom: calc(-90px + 0vw); left: 0;">'+
                                                     '<span id="star-1-dynamic_index" onclick="valorate(1);" title="No m\'agrada gens!" class="grow fa fa-star" style="cursor:pointer; font-size: calc(0.8em + 0.8vw);"></span>'+
