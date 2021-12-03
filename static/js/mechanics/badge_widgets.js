df3d184e5fc58280923d73401b5f595acf29aba0
Alexº <lexws33@gmail.com>
2021-12-03 20:39:44 +0100

    Deploy

--- static/js/mechanics/badge_widgets.js
+++ static/js/mechanics/badge_widgets.js
@@ -23,7 +23,7 @@ function log_txt_click(){
            // --------------------------------------------------------------------------
           };
 function choose_bdg(badge){
-	var locked_style = "";
+	var locked_style = "hellou";
     // if(badge[1]){
     //     locked_style = 'The last badge you won: ' + badge[0].title; 
     // }else{
