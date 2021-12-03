8fb2d47ed054fa2d822e08b4332d2d8eb89a9f12
Alexº <lexws33@gmail.com>
2021-12-03 17:41:44 +0100

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -232,8 +232,8 @@ try {
 																								 '.grow:hover { transform: scale(1.3); }</style>' + 
         										'<p style="position: relative; top: 0; left: 0;"> ' + 
         											'<img src="https://agmodule.herokuapp.com/media/avatars/" +  Has obtingut un premi:  </p>' +
-        										'<p style="position: absolute; bottom: calc(-70px + 1vw); left: 0;font-size:calc(1.5px + 1.5vw);">T\'ha agradat el premi?</p>' +
-        										'<p style="position: absolute; bottom: calc(-90px + 0vw); left: 0;">'+
+        										'<p style="position: absolute; bottom: calc(-70px + 0vw); left: 0;font-size:calc(1.5px + 1.5vw);">T\'ha agradat el premi?</p>' +
+        										'<br><p style="position: absolute; bottom: calc(-70px + 0vw); left: 0;">'+
                                                     '<span id="star-1-dynamic_index" onclick="valorate(1);" title="No m\'agrada gens!" class="grow fa fa-star" style="cursor:pointer; font-size: calc(0.8em + 0.8vw);"></span>'+
                                                     '<span id="star-2-dynamic_index" onclick="valorate(2);" title="No m\'agrada" class="grow fa fa-star" style="cursor:pointer; font-size: calc(0.8em + 0.8vw);"></span>'+
                                                     '<span id="star-3-dynamic_index" onclick="valorate(3);" title="M\'és indiferent" class="grow fa fa-star" style="cursor:pointer; font-size: calc(0.8em + 0.8vw);"></span>'+
