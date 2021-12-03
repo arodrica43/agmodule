7d56e4f66a9c7e37c14a00c3153b9563a9580523
Alexº <lexws33@gmail.com>
2021-12-03 22:05:22 +0100

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -248,7 +248,7 @@ try {
         .then(response => response.json())
         .then(res_json => (console.log(res_json), document.querySelector(id).innerHTML += '<style>.grow { transition: all .2s ease-in-out; }' +
 																								 '.grow:hover { transform: scale(1.3); }</style>' + 
-        										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;"> ' + 
+        										'<p class="grow" style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;"> ' + 
         											'<a href="' + link_url.replace(/\s/g, "+") + '" ><img style="width:10%;float:left; margin-right:calc(50px + 6vw);" src="https://agmodule.herokuapp.com/media/avatars/' + res_json.avatar.slice(res_json.avatar.indexOf(".") + 1, res_json.avatar.length) + '.png"></a></p>' +
         										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;left:calc(5% + 1vw);top:calc(2.5vw);"> ' + 
         											'<a href="' + link_url.replace(/\s/g, "+") + '" ><img style="width:5%;" src="https://agmodule.herokuapp.com/media/dashboard_icons/badges.png"></a></p>' +
