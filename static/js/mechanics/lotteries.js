ebde29ef01d09fcf0df2548fb16f8b9fd0a99405
Alexº <lexws33@gmail.com>
2021-12-16 15:44:44 +0100

    Deploy

--- static/js/mechanics/lotteries.js
+++ static/js/mechanics/lotteries.js
@@ -53,7 +53,7 @@ fetch("called_mechanic_url") // WARNING: should be replaced by a concrete mechan
     start_logs(log,"dynamic_user", "dynamic_mechanic_index",30);
     // ---------------------------------------------------------------------------------------
 
-  document.querySelector("#button_div").innerHTML = '<input class="btn btn-light" type="button" class="btn btn-primary" value="spin" id="spin" />';
+  document.querySelector("#button_div").innerHTML = '<button class="btn btn-light" type="button" value="Prova sort" id="spin" />';
   document.querySelector("#roulette_div").innerHTML = ' <canvas id="canvas" width="' + (window.innerWidth/2) + '" height="' + (window.innerHeight/2) + '"></canvas>';
   document.querySelector("#roulette_div").onclick = function(){
     // INTERACTION OCCURRENCE REGISTRATION --------------------------------------
