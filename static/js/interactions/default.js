7cc31bd941b9c9ab94bd7e3f1b40dddda541250a
Alexº <lexws33@gmail.com>
2021-12-22 12:57:11 +0100

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -252,7 +252,7 @@ try {
         											'<a href="' + link_url.replace(/\s/g, "+") + '" ><img style="width:10%;float:left; margin-right:calc(50px + 6vw);" src="https://agmodule.herokuapp.com/media/avatars/' + res_json.avatar.slice(res_json.avatar.indexOf(".") + 1, res_json.avatar.length) + '.png"></a></p>' +
         										'<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);width:100%;left:calc(5% + 1vw);top:calc(2.5vw);"> ' + 
         											'<a href="' + link_url.replace(/\s/g, "+") + '" ><img class="grow" style="width:5%;" src="https://agmodule.herokuapp.com/media/dashboard_icons/' + GMLabels[res_json.gmtype][1] + '.png"></a></p>' +
-                                                '<p style="position: absolute; bottom: calc(-60px + 0vw); right: 0;font-size:calc(10px + 1vw);">T\'ha agradat el premi?</p>' +   
+                                                '<p style="position: absolute; top: 0; right: 0;font-size:calc(10px + 1vw);"><img  src="https://agmodule.herokuapp.com/media/dashboard_icons/help.png></p>' +   
                                                 '<p style="position: absolute; bottom: calc(-60px + 0vw); right: 0;font-size:calc(10px + 1vw);">T\'ha agradat el premi?</p>' +	
         										'<p style="position: absolute; bottom: calc(-90px + 0vw); right: 0;z-index:2">'+
                                                     '<span id="star-1-dynamic_index" onclick="valorate(1);" title="No m\'agrada gens!" class="grow fa fa-star" style="cursor:pointer; font-size: calc(0.8em + 0.8vw);"></span>'+
