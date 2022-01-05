f88e7d1abc573cb6d821b32a7b1653c417540dc1
Alexº <lexws33@gmail.com>
2022-01-05 10:58:48 +0100

    Deploy

--- static/js/mechanics/social_statuses.js
+++ static/js/mechanics/social_statuses.js
@@ -105,7 +105,7 @@ document.querySelector("#incr-top-views").innerHTML = "En manteniment"
 //         console.log("Error: " + error);
 //     });
 
-fetch("https://agmodule.herokuapp.com/api/g_mechanics/133/?user=dynaimic_user&show_title=false&dynamic_index=followed")
+fetch("https://agmodule.herokuapp.com/api/g_mechanics/133/?user=dynaimic_user&?dynamic_user=dynamic_user&show_title=false&dynamic_index=followed")
                 .then(function (response) {
                     return response.json();
                 })
