3906ef8763c963572b34e495abfe28a0acb69143
Alexº <lexws33@gmail.com>
2022-01-07 17:10:47 +0100

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -17,7 +17,7 @@ try {
     }catch (cmserr){
         swal = import("https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.js");
     }
-    $(body).style += "body.swal2-shown:not(.swal2-no-backdrop):not(.swal2-toast-shown) { overflow-y: visible !important; }";
+    $('body').style += "body.swal2-shown:not(.swal2-no-backdrop):not(.swal2-toast-shown) { overflow-y: visible !important; }";
 
     //Main Time
     var tmp_main_t = 0;
