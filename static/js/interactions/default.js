25bfe217c406dfa0974b1a5ec996837eb4e55d44
Alexº <lexws33@gmail.com>
2022-01-07 15:49:43 +0100

    Deploy

--- static/js/interactions/default.js
+++ static/js/interactions/default.js
@@ -18,6 +18,8 @@ try {
         swal = import("https://cdn.jsdelivr.net/npm/sweetalert2@11.1.9/dist/sweetalert2.js");
     }
 
+    $('head').innerHTML += "<script type=\'application/javascript\' src=\'//cdn.jsdelivr.net/npm/sweetalert2@11\'></script>";
+
     //Main Time
     var tmp_main_t = 0;
     var main_timer;
