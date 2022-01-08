fc4f02b034e03884d7b00b25e089027fb872ba53
Alexº <lexws33@gmail.com>
2022-01-08 11:40:20 +0100

    Deploy

--- static/js/mechanics/gift_opener_widgets.js
+++ static/js/mechanics/gift_opener_widgets.js
@@ -19,14 +19,26 @@ function selectPolicy(list){
 	return list[0]
 }
 
-url = "https://agmodule.herokuapp.com/api/gift_openers/";
-fetch(url)
-.then(response => response.json())
-.then(res_json => (res_json.results))
-.then((list) => (selectPolicy(list))) // Select Policy: select the first (could be random, or other policy)
-.then((opener) => (console.log(opener), 
-						fetch("https://agmodule.herokuapp.com/api/g_mechanics/" + opener.id + "/?user=dynamic_user&show_title=false&dynamic_progress=dynamic_position")
-						.then(response => response.json())
-						.then((updated_opener) => (console.log(updated_opener), render_gop(updated_opener)))
-						.catch(error => (console.log("Error: " + error)))))
-.catch(error => (console.log("Error: " + error)))
+ fetch("https://agmodule.herokuapp.com/api/gamers/dynamic_user/add_gift?from=system&type=score&content=20") 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJson) {
+        
+        url = "https://agmodule.herokuapp.com/api/gift_openers/";
+        fetch(url)
+        .then(response => response.json())
+        .then(res_json => (res_json.results))
+        .then((list) => (selectPolicy(list))) // Select Policy: select the first (could be random, or other policy)
+        .then((opener) => (console.log(opener), 
+                                fetch("https://agmodule.herokuapp.com/api/g_mechanics/" + opener.id + "/?user=dynamic_user&show_title=false&dynamic_progress=dynamic_position")
+                                .then(response => response.json())
+                                .then((updated_opener) => (console.log(updated_opener), render_gop(updated_opener)))
+                                .catch(error => (console.log("Error: " + error)))))
+        .catch(error => (console.log("Error: " + error)))
+
+         
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+    });
