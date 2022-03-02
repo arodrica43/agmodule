3184ec8a92dd15da55bc9ecc738bceab45158ca0
arodrica43 <lexws33@gmail.com>
2022-03-02 19:57:25 +0100

    Update urls.py

--- agmodule/urls.py
+++ agmodule/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls import url
 from agmodule import settings
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
 # Swagger documentation setup
 schema_view = get_schema_view(
