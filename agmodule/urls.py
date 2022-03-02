881321153bac79b54cdc84e4edd1def0b27a7e5c
arodrica43 <lexws33@gmail.com>
2022-03-02 19:59:52 +0100

    Update urls.py

--- agmodule/urls.py
+++ agmodule/urls.py
@@ -74,7 +74,7 @@ urlpatterns = [
     path('api/statistics/get_interaction_index/<str:username>/<int:mechanic_id>', api.get_interaction_index), 
     path('api/statistics/get_current_valoration/<str:username>/<int:mechanic_id>', api.get_previous_valoration), 
     path('api/get_accessible_mechanics/<str:username>', api.get_accessible_mechanics),
-    path('docs/', include_docs_urls(title='Todo Api')),
+    #path('docs/', include_docs_urls(title='Todo Api')),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
